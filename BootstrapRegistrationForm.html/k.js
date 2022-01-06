17718c94d9df70cc5d16f596eb159e8d8152eb27
20b01a12g4_sahitya <20b01a12g4@svecw.edu.in>
2022-01-06 20:08:18 +0530

    third commit

--- BootstrapRegistrationForm.html/k.js
+++ BootstrapRegistrationForm.html/k.js
@@ -11,4 +11,7 @@ function validation(){
         if(sel.value === 'Branch'){
             document.getElementById("selid").innerHTML = "please select a branch"
         }
+        if(rad1.value == false && rad2.value== false && rad3.value == false){
+            document.getElementById("radid").innerHTML = "please select gender"
+        }
 }
